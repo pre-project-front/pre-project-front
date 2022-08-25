637f1d5fa01e3c182c6b72f10e1926fe777ae899
Jungyun park <104332913+mellomello-made@users.noreply.github.com>
2022-08-25 23:43:29 +0900

    FEAT.login 기능 구현 (#3)

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-function Login() {
-  return <div>Login!!!!</div>;
+function Login({ setIsLogin, setUserInfo }) {
+  const [loginInfo, setLoginInfo] = useState({
+    userId: "",
+    password: "",
+  });
+
+  const [checkedKeepLogin, setCheckedKeepLogin] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value });
+  };
+
+  const loginRequestHandler = () => {
+    if (!loginInfo.userId || !loginInfo.password) {
+      setErrorMessage("Email cannot be empty.");
+      return;
+    } else {
+      setErrorMessage("");
+    }
+    return axios
+      .post("https://localhost:3001/login", { loginInfo, checkedKeepLogin })
+      .then((res) => {
+        // 로그인에 성공했다면 응답으로 받은 데이터가 Mypage에 렌더링되도록 State를 변경하세요.
+        console.log(res.data);
+        setUserInfo(res.data);
+        setIsLogin(true);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setErrorMessage("Login failed");
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="left-box"></div>
+      <div className="right-box">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="input-field">
+            <span>Email</span>
+            <input
+              type="text"
+              data-testid="id-input"
+              onChange={handleInputValue("userId")}
+            />
+            <span>Password</span>
+            <input
+              type="password"
+              data-testid="password-input"
+              onChange={handleInputValue("password")}
+            />
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                onChange={() => setCheckedKeepLogin(!checkedKeepLogin)}
+              />
+              {"Keep Login"}
+            </label>
+          </div>
+          <button type="submit" onClick={loginRequestHandler}>
+            Log in
+          </button>
+          {errorMessage ? (
+            <div id="alert-message" data-testid="alert-message">
+              {errorMessage}
+            </div>
+          ) : (
+            ""
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
-
 export default Login;

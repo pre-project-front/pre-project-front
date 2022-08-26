ea4274889374e00c155a653f314e642ba37ef98b
lineJeong <103614912+lineJeong@users.noreply.github.com>
2022-08-26 13:53:45 +0900

    STYLE.question 페이지 관련 CSS구조작업 (#4)

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  height: 278px;
+  width: 100vw;
+  max-width: 100%;
+  position: absolute;
+  border: 1px black solid;
+`;
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return <FooterContainer>Footer</FooterContainer>;
 }
 
 export default Footer;

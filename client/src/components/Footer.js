78d0be6055335c587312aa3bb99a3cfad39e9b9b
lineJeong <line.jeong32@gmail.com>
2022-08-29 10:36:54 +0900

    STYLE:footer css 수정

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,7 +9,8 @@ export default Footer;
 
 const FooterContainer = styled.footer`
   background-color: hsl(210, 8%, 15%);
-  height: 322px;
+  min-height: 322px;
+  max-height: 322px;
   flex-grow: 0;
   padding: 32px 12px 12px 12px;
 `;

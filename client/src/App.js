2f11e3bd0d471eb7410bcc68a9b7d903e8a5402f
mellomello-made <mellomello.made@gmail.com>
2022-08-30 01:27:15 +0900

    FEAT. users page grid 레이아웃 생성

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Logout from "pages/Logout";
 import Signup from "pages/Signup";
 import { Route, Routes } from "react-router-dom";
 import EditForm from "pages/EditForm";
+import GridTest from "styles/GridTest";
 
 function App() {
   return (
